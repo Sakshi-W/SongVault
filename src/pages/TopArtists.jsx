40a31e12c8e9b03492b1ee831dc2e4d37851fe3c
Karina.Hedaoo <hedoos2055@gmail.com>
2023-07-01 17:22:49 +0530

    Changes by Karina

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,35 +1,25 @@
-import{ArtistCard, Loader, Error} from '../components';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { ArtistCard, Loader, Error } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const TopArtists = () => {
-    const {data, isFetching, error}=useGetTopChartsQuery();
+  const { data, isFetching, error, country } = useGetTopChartsQuery();
 
-     
-   
+  if (isFetching) return <Loader title="Loading Top Charts" />;
+  if (error && country) return <Error />;
 
-    if(isFetching) return <Loader title="Loading Top Charts"/>;
-    if(error && country) return <Error/>;
-
-    return(
-        <div className="flex flex-col">
-            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-                Discover Top Charts 
-                <span className="font-black">{country}</span>
-            </h2>
-            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-                {data?.map((track)=>(
-                <ArtistCard
-                    key={track.key}
-                    track={track}
-                    />
-
-                ))}
-
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Charts
+        <span className="font-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
+      </div>
+    </div>
+  );
 };
-    
 
-export default AroundYou;
+export default TopArtists;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,34 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-  reducerPath: 'shazamCoreApi',
+  reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam.p.rapidapi.com',
+    baseUrl: "https://shazam.p.rapidapi.com",
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '213e223760msh09840ec7b360906p16814fjsn59e6c2390a51');
+      headers.set(
+        "X-RapidAPI-Key",
+        "213e223760msh09840ec7b360906p16814fjsn59e6c2390a51"
+      );
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ queryFn: () => 'v1/charts/world' }),
-    getSongsByGenre: builder.query((genre) => ({
-      queryFn: () => `v1/charts/genre-world?genre_code=${genre}`,
-    })),
-    getSongsByCountry: builder.query((countryCode) => ({
-      queryFn: () => `v1/charts/country?country_code=${countryCode}`,
-    })),
-    getSongsBySearch: builder.query((searchTerm) => ({
-      queryFn: () => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
-    })),
-    getArtistDetails: builder.query((artistId) => ({
-      queryFn: () => `v2/artists/details?artist_id=${artistId}`,
-    })),
-    getSongDetails: builder.query(({ songid }) => ({
-      queryFn: () => `v1/tracks/details?track_id=${songid}`,
-    })),
-    getSongRelated: builder.query(({ songid }) => ({
-      queryFn: () => `v1/tracks/related?track_id=${songid}`,
-    })),
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      queryFn: (searchTerm) =>
+        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongDetails: builder.query({
+      query: (songid) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      queryFn: (songid) => `v1/tracks/related?track_id=${songid}`,
+    }),
   }),
 });
 
